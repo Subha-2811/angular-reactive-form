1947056daef48d92d911ef3756d9d215fbfd2d8a
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2022-06-09 19:24:48 +0530

    Added conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 // Creating Form Using FormBuilder
 import { FormBuilder, Validators } from '@angular/forms';
+
+// Cross Validation
 import { PasswordValidator } from './shared/password.validator';
 
 // Creating Form Using FormGroup and FormControl
@@ -13,18 +15,23 @@ import { PasswordValidator } from './shared/password.validator';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // Creating Form Using FormBuilder
-
   get username() {
-    console.log(this.registrationForm.get('username'));
     return this.registrationForm.get('username');
   }
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  // Creating Form Using FormBuilder
 
   constructor(private fb: FormBuilder) {}
 
   registrationForm = this.fb.group(
     {
-      username: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.email]],
+      subscribe: [false],
       password: [''],
       confirmPassword: [''],
       address: this.fb.group({
@@ -49,26 +56,44 @@ export class AppComponent {
   //   }),
   // });
 
+  // Conditional Validation
+
+  ngOnInit() {
+    this.registrationForm
+      .get('subscribe')
+      ?.valueChanges.subscribe((checked) => {
+        if (checked) {
+          this.email?.setValidators([Validators.required, Validators.email]);
+        } else {
+          this.email?.clearValidators();
+          this.email?.setValidators([Validators.email]);
+        }
+        this.email?.updateValueAndValidity();
+      });
+  }
+
   loadApiData() {
     // setValue() is strict and only allows us to add data if the structure completely matches
 
-    // this.registrationForm.setValue({
-    //   username: 'Subha',
-    //   password: 'test',
-    //   confirmPassword: 'test',
-    //   address: {
-    //     city: 'Jamshedpur',
-    //     state: 'Jharkhand',
-    //     postalCode: '831012',
-    //   },
-    // });
+    this.registrationForm.setValue({
+      username: 'Subha',
+      email: 'subhajyoti3845@gmail.com',
+      subscribe: false,
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'Jamshedpur',
+        state: 'Jharkhand',
+        postalCode: '831012',
+      },
+    });
 
     // If we need to add few of the fields then we can use patchValue()
 
-    this.registrationForm.patchValue({
-      username: 'Subha',
-      password: 'test',
-      confirmPassword: 'test',
-    });
+    // this.registrationForm.patchValue({
+    //   username: 'Subha',
+    //   password: 'test',
+    //   confirmPassword: 'test',
+    // });
   }
 }
